23c91f68fc1b0436abadb361d13d4b42dfe3c937
Отгонбаатар Баттөмөр <24LP0022@G401-CC0106.local>
2024-09-06 10:13:51 +0800

    zaza

--- src/components/pages/ContactUs.jsx
+++ src/components/pages/ContactUs.jsx
@@ -6,7 +6,7 @@ export default function ContactUs() {
     <main>
       <div className="flex flex-col w-auto h-screen gap-[100px]">
         <Header />
-        <div className="flex flex-col m-auto justify-center items-center h-auto sm:px-4 md:px-20 md:py-4  2xl:px-[350px]">
+        <div className="flex flex-col m-auto justify-center items-center h-auto sm:px-4 md:px-20 md:py-4 2xl:px-[350px]">
           <div className="flex flex-col justify-center items-center w-auto h-auto px-4 py-4 md:px-20 md:py-4 xl:px-[165px] gap-5">
             <div className="flex flex-col gap-5">
               <h1 className="text-black text-4xl font-semibold font-work-sans">
@@ -20,7 +20,7 @@ export default function ContactUs() {
                 dolore magna aliqua. Ut enim ad minim veniam
               </h1>
             </div>
-            <div className="flex w-auto h-auto justify-between gap-[50px] py-[10px]">
+            <div className="flex w-auto h-auto justify-between gap-4 md:gap-[50px] py-[10px]">
               <div className="flex flex-col w-auto h-auto p-4 rounded-xl border border-solid border-[#E8E8EA]">
                 <h1 className="text-black text-2xl font-semibold font-work-sans">
                   Address
